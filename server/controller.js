908addb41847580a421803de118e4b9d493eeb68
Isarel Getu <israelgetu101@gmail.com>
2022-03-01 22:50:53 -0600

    Well Done

--- server/controller.js
+++ server/controller.js
@@ -18,40 +18,30 @@ module.exports = {
   },
 
   updateMovie: (req, res) => {
-    //console.log(req.body, req.params);
     let { id } = req.params;
     let { type } = req.body;
+    let index = movies.findIndex((movie) => +movie.id === +id);
 
-    let index = movies.findIndex((movie) => id === +id);
     //console.log(index);
+    if (movies[index].rating === 5 && type === "plus") {
+      res.status(400).send("Cannot go above 5");
+    } else if (type === "plus") {
+      movies[index].rating++;
+      res.status(200).send(movies);
+    } else if (type === "minus" && movies[index].rating === 0) {
+      res.status(400).send("Cannot go below 0");
+    } else if (type === "minus") {
+      movies[index].rating--;
+      res.status(200).send(movies);
+    } else {
+      res.sendStatus(400);
+    }
+  },
+  //res.status(200).send(movies);
 
-    if (movies[index].rating === 5 && type 
-      === 'plus'){
-        res.status(400).send('Cannot go above 5')
-      }else if(type === 'plus'){
-        movie[index].rating++
-        res.status(200).send(movies)
-      }else if(type === 'minus' && movies[index].rating === 0){
-        res.status(400).send('Cannot go below 0')
-      }else is (type === 'minus'){
-        movies[index].rating--
-        res.status(200).send(movies)
-      }else{
-        res.sendStatus(400)
-      }
-      }
-    //res.status(200).send(movies);
-
-deleteMovie: (req, res) => {
-  let index = movies.findIndex(movie => +movie.id === +req.params.id)
-  movies.splice(index,1)
-  res.status(200).send(movies)
-}
-}
-
-
-
-
-
-  
-
+  deleteMovie: (req, res) => {
+    let index = movies.findIndex((movie) => +movie.id === +req.params.id);
+    movies.splice(index, 1);
+    res.status(200).send(movies);
+  },
+};
